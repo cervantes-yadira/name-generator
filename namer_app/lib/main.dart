8ea4cc5c3027da3e3a5571eaca3ca5c2cfbd25c5
Yadira Cervantes <githubusername@noreply.github.com>
2024-06-25 20:49:29 -0700

    Create Flutter app, add functional button, and change basic styling

---
+++ namer_app/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// runs the app
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // dictates what's within the app
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData( // some stylistic theme
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange), // provides a theme compatible with a given color
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+// manages the state of the app
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();// updates app each time MyAppState changes
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Center( // centers column horizontally
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // main axis is the vertical axis
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary, // provides a high contrast color against primary color
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase, 
+          style: style,
+          semanticsLabel: pair.asPascalCase, // for accessibility, helps screen readers
+        ),
+      ),
+    );
+  }
+}
